bbf9791b4652040f9c02e3f41cfb72b504d38453
VeryrrDefine <1308061599@qq.com>
2026-01-31 15:23:14 +0800

    formated

--- hotkeys.js
+++ hotkeys.js
@@ -1,19 +1,20 @@
-
 "use strict";
 
 const keybinds = {
-    i: () => infinity(1),
-    m: () => {
-      maxall()
-      maxFactors()
-    },
-    s: () => factorShift(),
-    d: () => (game.diagonalizeUnlock==1 ? diagonalize() : 0),
-    r: () => {if (game.boostUnlock === 1) refund()}
+  i: () => infinity(1),
+  m: () => {
+    maxall();
+    maxFactors();
+  },
+  s: () => factorShift(),
+  d: () => (game.diagonalizeUnlock == 1 ? diagonalize() : 0),
+  r: () => {
+    if (game.boostUnlock === 1) refund();
+  },
 };
 
 // Declaring it once is probably faster
-window.onkeypress = _ => {
+window.onkeypress = (_) => {
   const k = _.key.toLowerCase();
   if (keybinds[k]) keybinds[k]();
 };
